bbdc5d301eb14038af07f78c3859d000dcc33200
Lars Kellogg-Stedman <lars@redhat.com>
2021-07-13 14:33:59 -0400

    overwrite files when extracting

--- extract-iso.sh
+++ extract-iso.sh
@@ -5,7 +5,7 @@ discovery_iso=$1
 [ -f "$discovery_iso" ] || exit 1
 
 mkdir images
-isoinfo -i $discovery_iso -x '/IMAGES/IGNITION.IMG;1' | zcat | cpio -ivd
+isoinfo -i $discovery_iso -x '/IMAGES/IGNITION.IMG;1' | zcat | cpio -iuvd
 isoinfo -i $discovery_iso -x '/IMAGES/PXEBOOT/INITRD.IMG;1' > images/initrd.img
 isoinfo -i $discovery_iso -x '/IMAGES/PXEBOOT/ROOTFS.IMG;1' > images/rootfs.img
 isoinfo -i $discovery_iso -x '/IMAGES/PXEBOOT/VMLINUZ.;1' > images/vmlinuz
