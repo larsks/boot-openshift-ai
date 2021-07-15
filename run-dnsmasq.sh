3c8dbabef1c27b31b6004bd199f3d96c2d18416d
Lars Kellogg-Stedman <lars@redhat.com>
2021-07-15 10:03:19 -0400

    write pid file

--- run-dnsmasq.sh
+++ run-dnsmasq.sh
@@ -3,6 +3,7 @@
 . ./config.sh
 
 exec dnsmasq -d \
+	--pid-file=dnsmasq.pid \
 	-i $PROVISIONING_INTERFACE \
 	--bind-interfaces \
 	--dhcp-range=$DHCP_RANGE_BEGIN,$DHCP_RANGE_END,$DHCP_LEASE_TIME \
