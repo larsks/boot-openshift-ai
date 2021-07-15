abb23ca3a4262c5ebe6e01f5087f68102233d5d6
Lars Kellogg-Stedman <lars@redhat.com>
2021-07-15 09:58:31 -0400

    add bind-interfaces

--- run-dnsmasq.sh
+++ run-dnsmasq.sh
@@ -2,7 +2,9 @@
 
 . ./config.sh
 
-exec dnsmasq -d -i $PROVISIONING_INTERFACE \
+exec dnsmasq -d \
+	-i $PROVISIONING_INTERFACE \
+	--bind-interfaces \
 	--dhcp-range=$DHCP_RANGE_BEGIN,$DHCP_RANGE_END,$DHCP_LEASE_TIME \
 	--enable-tftp \
 	--tftp-root=$PWD \
